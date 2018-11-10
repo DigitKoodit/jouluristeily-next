1be674d4171bc82ca8fcc41dd2590a5dd5be4035
Matti Liikala <matti.liikala@sofokus.com>
2018-11-10 16:04:58 +0200

    styling, rendering

---
+++ styles/stylesheet.ts
@@ -0,0 +1,15 @@
+export const colors = {
+    red: '#EF1424',
+    lightred: '#F4383E',
+    yellow: '#FFD000',
+    lightyellow: '#FFE578',
+    black: '#303030',
+    white: '#FFFDFD'
+}
+
+export const fonts = {
+    title: 'Passion One',
+    secondarytitle: 'Patua One',
+    paragraph: 'Open Sans',
+}
+

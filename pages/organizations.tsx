3b109f6c993d4d649285bca4bce5eca03bdfc341
Julius Rajala <juliusrajala@gmail.com>
2018-11-10 13:55:57 +0200

    Component example.

---
+++ pages/organizations.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+
+class OrganizationPage extends React.Component {
+  state = { data: {} }
+  getInitialProps() {
+    // Fetch data;
+  }
+
+  render() {
+    return <div></div>
+  }
+}

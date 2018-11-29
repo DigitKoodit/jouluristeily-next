c3bca976629d7d805bc33f025a115177a6a108c7
Julius Rajala <juliusrajala@gmail.com>
2018-11-29 02:05:11 +0200

    Custom server-setup to allow for service worker usage.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 const withTypescript = require('@zeit/next-typescript')
+const withOffline = require('next-offline');
 const webpack = require('webpack');
 require('dotenv').config();
 
-module.exports = withTypescript({
+module.exports = withTypescript(withOffline({
   webpack: config => {
     const env = Object.keys(process.env).reduce((acc, curr) => {
       acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
@@ -12,4 +13,4 @@ module.exports = withTypescript({
     config.plugins.push(new webpack.DefinePlugin(env));
     return config;
   }
-});
+}));
